a347ba55043f2a274b08c0202e137941be196bea
Ayesha Shafeeqa <ayeshass.2019@sis.smu.edu.sg>
2020-11-06 01:17:42 +0800

    Updata database with new tables

--- database.sql
+++ database.sql
@@ -12,11 +12,25 @@ CREATE TABLE if not exists donoraccount (
   mrt varchar(256) NOT NULL
 );
 
+CREATE TABLE if not exists donorlisting (
+  username varchar(256) NOT NULL,
+  mrt varchar(256) NOT NULL,
+  category varchar(256) NOT NULL,
+  item varchar(256) NOT NULL,
+  quantity varchar(256) NOT NULL,
+  itemcondition varchar(256) NOT NULL
+);
+
 CREATE TABLE if not exists beneficiaryaccount (
   username varchar(20) NOT NULL,
   passcode varchar(256) NOT NULL, -- change to hashed password?
   email varchar (256) NOT NULL,
   phonenumber int NOT NULL,
+  mrt varchar(256) NOT NULL
+);
+
+CREATE TABLE if not exists beneficiarylisting (
+  username varchar(20) NOT NULL,
   mrt varchar(256) NOT NULL,
   category varchar(256) NOT NULL,
   item varchar(256) NOT NULL,
@@ -25,8 +39,14 @@ CREATE TABLE if not exists beneficiaryaccount (
   comments varchar(500)
 );
 
-INSERT INTO beneficiaryaccount (username, passcode, email, phonenumber, mrt, category, item, quantity, wantedby, comments) VALUES
-('Zack', 'ilovecoding122', 'zack&cody@gmail.com', '98764321', 'Paya Lebar', 'Hygiene', 'Diapers', '2 packs', '2020/11/12', 'Merries Tape Diapers - S' ),
-('Thomas', 'baby98', 'thomastrain@yahoo.com.sg', '87645441', 'Boon Lay', 'Clothing', 'Shirt for 3 month old boy', '3', '2020/11/29', 'Dark colour please'),
-('Wong Shi Lin', 'heeehaw$$', 'shilinfang@hotmail.com', '86774333', 'Sengkang', 'Toys', 'Rattle toy', '5', '2020/11/15', ''),
-('Tammy Ho', 'hahahaha@88', 'tammythehamster@gmail.com', '62352345', 'Bedok', 'Hygiene', 'Baby shampoo', '2 bottles', '2020/11/30', 'Dove baby shampoo - rich moisture');
+INSERT INTO beneficiaryaccount (username, passcode, email, phonenumber, mrt) VALUES
+('Zack', 'ilovecoding122', 'zack&cody@gmail.com', '98764321', 'Paya Lebar'),
+('Thomas', 'baby98', 'thomastrain@yahoo.com.sg', '87645441', 'Boon Lay' ),
+('Wong Shi Lin', 'heeehaw$$', 'shilinfang@hotmail.com', '86774333', 'Sengkang'),
+('Tammy Ho', 'hahahaha@88', 'tammythehamster@gmail.com', '62352345', 'Bedok');
+
+INSERT INTO beneficiarylisting (username, mrt, category, item, quantity, wantedby, comments) VALUES 
+('Zack', 'Paya Lebar', 'Hygiene', 'Diapers', '2 packs', '2020/11/12', 'Merries Tape Diapers - S' ),
+('Thomas', 'Boon Lay', 'Clothing', 'Shirt for 3 month old boy', '3', '2020/11/29', 'Dark colour please'),
+('Wong Shi Lin','Sengkang' , 'Toys', 'Rattle toy', '5', '2020/11/15', ''),
+('Tammy Ho', 'Bedok' , 'Hygiene', 'Baby shampoo', '2 bottles', '2020/11/30', 'Dove baby shampoo - rich moisture');
