3b1bf0d67bfd5dac670aa0647ec86929b9a920e4
isaacfong1998 <73023147+isaacfong1998@users.noreply.github.com>
2020-11-06 14:13:06 +0800

    Update ConnectionManager.php

--- ConnectionManager.php
+++ ConnectionManager.php
@@ -6,7 +6,7 @@ class ConnectionManager {
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $dbname = "userDB";
+        $dbname = "BBB_database";
         
         // Create connection
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);     
