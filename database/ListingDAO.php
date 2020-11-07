32fc455c3c8de1ef85a448e84712dee211c403ac
Ayesha Shafeeqa <ayeshass.2019@sis.smu.edu.sg>
2020-11-07 17:18:18 +0800

    Add category method to listingDAO

--- database/ListingDAO.php
+++ database/ListingDAO.php
@@ -9,19 +9,37 @@
   class ListingDAO {
 
     public function getListing() {
-          $connMgr = new ConnectionManager();      
-          $pdo = $connMgr->connect();  
-          $sql = 'SELECT username, category, item, mrt FROM beneficiarylisting';         
-          $stmt = $pdo->prepare($sql);      
           $stmt->execute();			
-          $result = [];
-          $stmt->setFetchMode(PDO::FETCH_ASSOC);
-          while($row = $stmt->fetch()) {
-            $result[] = new Listing($row['username'], $row['category'], $row['item'], $row['mrt']);
-          }
-          $stmt = null;
-          $pdo = null;
-          return $result;
+      $connMgr = new ConnectionManager();      
+      $pdo = $connMgr->connect();  
+      $sql = 'SELECT username, category, item, mrt FROM beneficiarylisting';         
+      $stmt = $pdo->prepare($sql);      
+      $stmt->execute();			
+      $result = [];
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      while($row = $stmt->fetch()) {
+        $result[] = new Listing($row['username'], $row['category'], $row['item'], $row['mrt']);
+      }
+      $stmt = null;
+      $pdo = null;
+      return $result;
+    }
+
+    public function getListingByCat($category) {
+      $connMgr = new ConnectionManager();      
+      $pdo = $connMgr->connect();  
+      $sql = 'SELECT username, category, item, mrt FROM beneficiarylisting where category = :category';         
+      $stmt = $pdo->prepare($sql);   
+      $stmt->bindParam(':category', $category, PDO::PARAM_STR);     
+      $stmt->execute();			
+      $result = [];
+      $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      while($row = $stmt->fetch()) {
+        $result[] = new Listing($row['username'], $row['category'], $row['item'], $row['mrt']);
+      }
+      $stmt = null;
+      $pdo = null;
+      return $result;
     }
   }
 ?>
