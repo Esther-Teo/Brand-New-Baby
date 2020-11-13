8008e306ad5886b768e13731f42342c192a84d83
isaacfong1998 <73023147+isaacfong1998@users.noreply.github.com>
2020-11-13 14:07:25 +0800

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -39,3 +39,14 @@ INSERT INTO beneficiarylisting (username, mrt, category, item, quantity, itemcon
 ('Thomas', 'Boon Lay', 'Clothing', 'Shirt for 3 month old boy', '3', 'preLoved'),
 ('Wong Shi Lin','Sengkang' , 'Toys', 'Rattle toy', '5', 'preLoved'),
 ('Tammy Ho', 'Bedok' , 'Hygiene', 'Baby shampoo', '2 bottles', 'newCondition');
+('Timothy', 'RedHill' , 'Clothing', 'baby pants', '2 pairs', 'used or new');
+('Jasmine Tan', 'Ang Mo Kio', 'Toys', 'Robot toy', '1', '2020/2/12/','iron man')
+('Maximus Lim', 'Jurong East' , 'Clothing', 'baby pants', '2 pairs', 'used or new');
+('Jamie Lee', 'Macpherson' , 'Toys', 'Baby puzzles', '1 set', '');
+('Siu Lee', 'Yishun' , 'Hygiene', 'Baby bathing powder', '2 bottles', '');
+('Jackson Teo', 'City Hall' , 'Toys', 'Superhero figurines', '4', 'power ranger figurines preferred');
+('Kimberly Ann', 'Choa Chu Kang' , 'Hygiene', 'Pacifier', '2', 'new with packaging');
+('Hoe Hin', 'Simei' , 'Clothing', 'Baby socks', '5 pairs', 'new condition');
+('Darryl', 'Eunos' , 'Toys', 'Dinosaur figurine', '3 or more sets', 'prefers Trex');
+('Wee Liang', 'Queenstown' , 'Hygiene', 'Baby Diapers', '2 packages', 'Huggies brand ');
+('Amos Lee', 'Raffles Place' , 'Hygiene', 'Baby body wash', '6 bottles', '');
